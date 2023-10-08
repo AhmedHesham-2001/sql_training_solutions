369e3f738306b2e0a35ba2d26c50d5307dff8ca2
AhmedHesham-2001 <85707360+AhmedHesham-2001@users.noreply.github.com>
2023-10-08 18:40:31 +0300

    Create Teams Power Users [Microsoft SQL Interview Question].sql

---
+++ Teams Power Users [Microsoft SQL Interview Question].sql
@@ -0,0 +1,10 @@
+/* this is the solution to the "Teams Power Users [Microsoft SQL Interview Question]"
+
+the link to this Question : https://datalemur.com/questions/teams-power-users
+*/
+
+SELECT sender_id , COUNT(message_id )as message_count FROM messages
+WHERE sent_date BETWEEN '2022-08-01' AND '2022-08-31' /* there is many way to write this line */
+GROUP BY sender_id
+ORDER BY message_count DESC
+LIMIT 2 ;
